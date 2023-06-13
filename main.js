5dde8c327735a49b197b289a1de9e8d2f0eca23e
Marcelo Zapatta <marcelozapatta0@gmail.com>
2023-06-12 21:58:36 -0300

    feat: unclickble button

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 import './style.css'
+import UnclickableButton from  './unclickable-button.js';
+
+const button = document.querySelector('button');
+const unclickableButton = new UnclickableButton(button);
